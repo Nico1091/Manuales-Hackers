c618e24d3a9ca0954f890721db77ddad9634a496
Nico1091 <ni1091352918@gmail.com>
2024-07-21 11:35:45 -0500

    Cambios nuevos

--- Cajero/src/cajero/Cajero.java
+++
@@ -1,9 +0,0 @@
-package cajero;
-public class Cajero {
-    public static void main(String[] args) throws InterruptedException {
-        // TODO code application logic here
-        Funciones_cajero llamar = new Funciones_cajero();
-        llamar.tiempo();
-        llamar.num();
-    }
-}
